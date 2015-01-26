cd918c34d9d7b2a75b0685b9192755b3174c4826
Lars <K3LARRA@HOTMAIL.COM>
2015-01-26 11:38:25 +0100

    added some cool and new stuff

--- Plants_vs_Zombies/src/se/mah/k3/pfi2/Controller.java
+++ Plants_vs_Zombies/src/se/mah/k3/pfi2/Controller.java
@@ -1,18 +1,25 @@
 package se.mah.k3.pfi2;
 
+import java.util.ArrayList;
+
 public class Controller {
 	public Controller() {
 		GamePanel gamePanel = new GamePanel ();
 		gamePanel.setVisible(true);
-		// TODO Auto-generated constructor stub
+		ArrayList<Zombie> zombies = new ArrayList<Zombie>();
+
 		Zombie testZombie= new Zombie("Zombie1", 50, 100, "NormalZombie");
 		Zombie testZombie2= new Zombie("Zombie2", 30, 120, "MrZombie");
 		
-		String s = testZombie.getName();
-		System.out.println("Namnet på zombien är "+ s);
-		
-		s = testZombie2.getName();
-		System.out.println("Namnet på zombien är "+ s);
+		zombies.add(testZombie);
+		zombies.add(testZombie2);
+		zombies.add(new Zombie("Zombie3", 50, 100, "NormalZombie"));
+		zombies.add(new Zombie("Zombie4", 50, 100, "SjukZombie"));
+		zombies.add(new Zombie("Zombie5", 50, 100, "MrZombie"));
+		zombies.add(new Zombie("Zombie6", 50, 100, "MrZombie"));
+		for (Zombie zombie : zombies) {
+			System.out.println("Namn: "+zombie.getName());
+		}
 		
 	}
 
