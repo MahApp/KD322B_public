4dc417a1f3d2dbfdae0ba3e0603c1249b7c57e26
Lars <K3LARRA@HOTMAIL.COM>
2015-01-26 13:45:05 +0100

    Added UML diagram

--- Plants_vs_Zombies/src/se/mah/k3/pfi2/Controller.java
+++ Plants_vs_Zombies/src/se/mah/k3/pfi2/Controller.java
@@ -13,10 +13,10 @@ public class Controller {
 		
 		zombies.add(testZombie);
 		zombies.add(testZombie2);
-		zombies.add(new Zombie("Zombie3", 50, 100, "NormalZombie"));
-		zombies.add(new Zombie("Zombie4", 50, 100, "SjukZombie"));
-		zombies.add(new Zombie("Zombie5", 50, 100, "MrZombie"));
-		zombies.add(new Zombie("Zombie6", 50, 100, "MrZombie"));
+		zombies.add(new Zombie("Zombie3", 50, 99, "NormalZombie"));
+		zombies.add(new Zombie("Zombie4", 70, 155, "SjukZombie"));
+		zombies.add(new Zombie("Zombie5", 87, 62, "MrZombie"));
+		zombies.add(new Zombie("Zombie6", 12, 13, "MrZombie"));
 		for (Zombie zombie : zombies) {
 			System.out.println("Namn: "+zombie.getName());
 		}
