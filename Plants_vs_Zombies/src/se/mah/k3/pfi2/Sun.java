73b5d3856102a47b29db29d9303acb34ceb3653b
Philip <philnord.94@gmail.com>
2015-02-09 11:58:22 +0100

    Improved sun class

--- Plants_vs_Zombies/src/se/mah/k3/pfi2/Sun.java
+++ Plants_vs_Zombies/src/se/mah/k3/pfi2/Sun.java
@@ -1,17 +1,22 @@
 package se.mah.k3.pfi2;
 
+import java.util.Random;
+
 public class Sun {
 
 	private int positionX;
 	private int positionY;
-	
+	Random rand = new Random();
+	int r = rand.nextInt(500);
+	int r2 = rand.nextInt(350);
 	public Sun(int positionXIn, int positionYIn) {
 		
-		this.positionX = positionXIn;
+		this.positionX = positionXIn + r;
 		this.positionY = positionYIn;
 	}
 
 	public int getPositionX() {
+		
 		return positionX;
 	}
 
@@ -21,6 +26,9 @@ public class Sun {
 
  
 	public void fall(){
+		
+		if(this.positionY < r2){
 		this.positionY = this.positionY + 2;
+		}
 	}
 }
