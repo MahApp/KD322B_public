a2c2af855d3c3ede7e6b1b6c585c15f570a87623
Mattias Andersson <mattias8012@gmail.com>
2015-02-19 16:17:38 +0100

    Har lagt in en bild för cherry bomb. Skulle vilja göra så den försvinner efter x antal sekunder men lyckas inte riktigt skapa en räknare inom klassen som håller koll på tiden.

--- Plants_vs_Zombies/src/se/mah/k3/pfi2/model/Cherry.java
+++ Plants_vs_Zombies/src/se/mah/k3/pfi2/model/Cherry.java
@@ -9,7 +9,7 @@ import java.util.Random; // Import the random library
 
 public class Cherry extends Plant {
 	private Image image;
-	private int shake=250;
+	private int shake = 250;
 
 	Random rand = new Random();
 	int randomPositionY = rand.nextInt((390 - 0)); // Generate a random number
@@ -22,7 +22,14 @@ public class Cherry extends Plant {
 		this.setPositionY(0); // set positionY to 0 for cheery to spawn at the
 								// top of the screen
 		this.setPositionX(250);
-		image = Toolkit.getDefaultToolkit().getImage(Cherry.class.getResource("/se/mah/k3/pfi2/images/cherrybombz.png")); // add custom image for cherry bomb!
+		image = Toolkit.getDefaultToolkit().getImage(
+				Cherry.class
+						.getResource("/se/mah/k3/pfi2/images/cherrybombz.png")); // add
+																					// custom
+																					// image
+																					// for
+																					// cherry
+																					// bomb!
 
 	}
 
@@ -31,30 +38,21 @@ public class Cherry extends Plant {
 		// TODO Auto-generated method stub
 		return image;
 	}
-	
+
 	@Override
 	public void act() {
 
 		// if positionY is less than the random number in randomPositionY, then
 		// add 2 to cherry's positionY until positionY is equal to the random
 		// generated number in randomPositionY
-		
-		if (getPositionY() < randomPositionY) {
-			this.setPositionY(getPositionY() + 2);
-		}
-	
+
 	}
-	
 
 	@Override
 	public void doYourThing() {
-		if (getPositionY() < randomPositionY) {
+		if (this.getPositionY() < this.randomPositionY) {
 			this.setPositionY(getPositionY() + 2);
-	
-		}
-
-		
+			
+}
 	}
-
-
 }
