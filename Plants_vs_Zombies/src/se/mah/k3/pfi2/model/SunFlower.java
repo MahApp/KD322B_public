ade5bde06b08cc78beec7de42e08dfea9674a3b7
lstenstadvold <lstenstadvold@gmail.com>
2015-02-20 12:47:45 +0100

    Trying to add a function where a sun is created after ten seconds. See comment in SunFlower.doYourThing().

--- Plants_vs_Zombies/src/se/mah/k3/pfi2/model/SunFlower.java
+++ Plants_vs_Zombies/src/se/mah/k3/pfi2/model/SunFlower.java
@@ -2,36 +2,48 @@ package se.mah.k3.pfi2.model;
 
 import java.awt.Image;
 import java.awt.Toolkit;
-
-
+import java.util.Timer;
 import java.util.Random;
 
+import se.mah.k3.pfi2.control.Controller;
 import se.mah.k3.pfi2.view.GamePanel;
+
+import java.util.Timer;
 
 public class SunFlower extends Plant {
 
 	private Image image;
-	
+	private Sun sun;
+	private int sunTime = 1;
+	private int posX;
+	private int posY;
+	private Controller controller;
 	
 	public SunFlower(int positionXIn, int positionYIn) {
 		super(positionXIn, positionYIn);
+		posX = positionXIn;
+		posY = positionYIn;
 		image = Toolkit.getDefaultToolkit().getImage(SunFlower.class.getResource("/se/mah/k3/pfi2/images/Sunflower1.png")); //added our Sunflower image to the game. Anton
 	}
 
 	@Override
 	public void act() {
-		Random random = new Random();
-	setPositionX(random.nextInt(100)+1);						//A Sunflower, from what I can understand, remains on its position. Anton
-	setPositionY(random.nextInt(100)+1);						//I had an idea of trying to randomize the positions when clicking the btn, not sure if this works. Anton
-																
-	//add new sun after specified time << This is left to do
+					
 	}
 
 	@Override
 	public void doYourThing() {
-		Random random = new Random();
-		setPositionX(random.nextInt(100)+1);						//A Sunflower, from what I can understand, remains on its position. Anton
-		setPositionY(random.nextInt(100)+1);	
+		
+		sunTime ++;
+
+		System.out.println(sunTime);
+		if(sunTime==1000){
+			controller.addSun(); //nullpointer! We don't understand why we can't do this.
+			//create sun here. 
+		}else{
+			
+		}
+		
 	}
 
 	@Override
