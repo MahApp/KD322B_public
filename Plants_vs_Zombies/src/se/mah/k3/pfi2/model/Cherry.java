d3adaa15703c1c80a6b4a5e4bf7205b22ce43ed2
Mattias Andersson <mattias8012@gmail.com>
2015-02-20 19:43:56 +0100

    # Cherrybomb spawar nu på ett random x-kordinat och faller sedan till ett random y-kordinat.
    # När det landat på det slumpmässiga y-kordinatet så ligger det i någon
    sekund innan bilden byts ut mot en explosion för att sedan försvinna.
    
    # När cherrybomben "landat" så startar en räknare som används dels för
    att ta bort bilden av cherrybomben efter x antal seknder och dels för
    att stoppa loopen som finns i metodne doYoutThing — loopen körs bara så
    länge räknarens värde är under ett satt värde. Detta för att inte
    räknaren skall leva vidare och fortsätta öka och dra cpu-kraft. Det
    optimala skulle vara att döda instansen av cherrybomben helt och hållet
    efter explosionen men detta får duga tills vidare!

--- Plants_vs_Zombies/src/se/mah/k3/pfi2/model/Cherry.java
+++ Plants_vs_Zombies/src/se/mah/k3/pfi2/model/Cherry.java
@@ -11,7 +11,6 @@ public class Cherry extends Plant {
 	private Image image;
 	private Image invisibility;
 	private Image explosion;
-	
 
 	// counter for cherrybomb's lifetime.
 	int aliveTime = 0;
@@ -20,10 +19,8 @@ public class Cherry extends Plant {
 	Random rand2 = new Random();
 
 	// Random numbers for x & y positions.
-	int randomPositionY = rand.nextInt((390 - 0)); 
+	int randomPositionY = rand.nextInt((450 - 0));
 	int randomPositionX = rand2.nextInt((700 - 0));
-													
-													
 
 	public Cherry(int positionXIn, int positionYIn) {
 		super(positionXIn, positionYIn);
@@ -45,12 +42,11 @@ public class Cherry extends Plant {
 
 		Cherry.class.getResource("/se/mah/k3/pfi2/images/cherrybombz.png"));
 
-		
+		// load explosion image
 		this.explosion = Toolkit.getDefaultToolkit().getImage(
 
 		Cherry.class.getResource("/se/mah/k3/pfi2/images/Explodez.png"));
-		
-		
+
 	}
 
 	@Override
@@ -66,30 +62,34 @@ public class Cherry extends Plant {
 
 	@Override
 	public void doYourThing() {
+		if (aliveTime < 631) {
 
-		// Set position Y to it's current position +1 until it's equal to
-		// randomPositionY.
-		if (this.getPositionY() < this.randomPositionY) {
-			this.setPositionY(getPositionY() + 1);
+			// Set position Y to it's current position +1 until it's equal to
+			// randomPositionY.
+			if (this.getPositionY() < this.randomPositionY) {
+				this.setPositionY(getPositionY() + 1);
 
-		}
+			}
 
-		// When cherrybomb position is equal to randomposition Y it has landed.
-		// Start counter "aliveTime" in order to remove it after x seconds.
-		if (this.getPositionY() == randomPositionY) {
-			aliveTime = aliveTime + 1;
+			// When cherrybomb position is equal to randomposition Y it has
+			// landed.
+			// Start counter "aliveTime" in order to remove it after x seconds.
+			if (this.getPositionY() == randomPositionY) {
+				aliveTime = aliveTime + 1;
 
-		}
+			}
 
-		// When aliveTime has counted to 1000, change cherrybomb image to
-		// invisible (transparent) image.
-		if (aliveTime == 1000) {
-			this.image = (explosion);
-		}
-		if (aliveTime == 120	0) {
-			this.image = (invisibility);
-			//this.image = (invisibility);
-
+			// When aliveTime has counted to 1000, change cherrybomb image to
+			// invisible (transparent) image.
+			if (aliveTime == 600) {
+				// adjusting x position to fit explosion.
+				setPositionX(getPositionX() - 50);
+				this.image = (explosion);
+			}
+			// change to invislbe image when timer is at 1050.
+			if (aliveTime == 630) {
+				this.image = (invisibility);
+			}
 		}
 
 	}
