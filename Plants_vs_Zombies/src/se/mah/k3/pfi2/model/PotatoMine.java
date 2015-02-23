4035bfedbdeda0e90e078673fdfc4a4623c83054
Manne Westermark <mannewestermark@gmail.com>
2015-02-23 08:27:56 +0100

    bakgrunden loopas över bilden hela tiden

--- Plants_vs_Zombies/src/se/mah/k3/pfi2/model/PotatoMine.java
+++ Plants_vs_Zombies/src/se/mah/k3/pfi2/model/PotatoMine.java
@@ -1,22 +1,27 @@
 package se.mah.k3.pfi2.model;
 
 import java.awt.Image;
+import java.awt.Toolkit;
 
 public class PotatoMine extends Plant {
 
 	private int move = 0;
 	private boolean moving = false;
+	private Image image;
 
 
 		
 	public PotatoMine(int positionXIn, int positionYIn) {
 		super(positionXIn, positionYIn);
+		
+		image = Toolkit.getDefaultToolkit().getImage(PeaShooter.class.getResource("/se/mah/k3/pfi2/images/potatomine.png"));
 
 
 	}
 
 	@Override
 	public void doYourThing() {
+		getImage();
 		setPositionX(getPositionX() + move);
 		setPositionY(getPositionY());
 		if(moving == true){
@@ -36,6 +41,6 @@ public class PotatoMine extends Plant {
 	@Override
 	public Image getImage() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.image;
 	}
 }
