e98f24857209f0544f98571de6fc09fa2f41cac2
dhamark <dhamark@gmail.com>
2015-02-26 13:42:53 +0100

    Walnut class: image + setposition

--- Plants_vs_Zombies/src/se/mah/k3/pfi2/model/Walnut.java
+++ Plants_vs_Zombies/src/se/mah/k3/pfi2/model/Walnut.java
@@ -1,24 +1,40 @@
 package se.mah.k3.pfi2.model;
 
 import java.awt.Image;
+import java.awt.Toolkit;
+import java.util.Random;
 
 public class Walnut extends Plant {
+private Image image; 
+Random rand = new Random();
+Random rand2 = new Random();
+
+//random position
+int randomPositionY = rand.nextInt(450);
+int randomPositionX = rand2.nextInt(900);
 
 	public Walnut(int positionXIn, int positionYIn) {
 		super(positionXIn, positionYIn);
+		this.setPositionY(0);
+		this.setPositionX(100);
+		
+this.setPositionX(this.randomPositionX);
+this.setPositionY(this.randomPositionY);
 
+//Load walnut image
+		this.image = Toolkit.getDefaultToolkit().getImage(Walnut.class.getResource("/se/mah/k3/pfi2/images/walnut.png"));
 	}
 
 	@Override
 	public void doYourThing() {
-		setPositionX(getPositionX()+1);
-		this.setPositionY(getPositionY()+1);
+		setPositionX(getPositionX());
+		this.setPositionY(getPositionY());
 	}
 
 	@Override
 	public Image getImage() {
 		// TODO Auto-generated method stub
-		return null;
+		return image;
 	}
 
 }
